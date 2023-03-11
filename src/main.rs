9944856241f1c294121797669bfaa1cfa247b9cc
rahulan-mahendra <rahulmahendra24@gmail.com>
2023-03-11 14:16:23 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // We have orders for three new cars!
+    // We'll declare a mutable car variable and reuse it for all the cars
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
+}
+
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, trasmission: Transmission, convertible: bool, ) -> Car {
+    let car = Car { color: String::from(color), transmission: trasmission, convertible: convertible, mileage: 50};
+    car
+}
+
